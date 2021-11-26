ed1d5a1a35792d70bd52dd47cbb982d7665f9313
naresh0839 <naresh0839@gmail.com>
2021-11-26 17:04:56 +0530

    setting up environment

--- lorax/lorax/settings.py
+++ lorax/lorax/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'railway_management',
         'USER': 'root',
-        'PASSWORD': '12345678',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
