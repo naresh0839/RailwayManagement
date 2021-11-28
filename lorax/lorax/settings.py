bd0c7190d55e6ab81c2e39d719412bab52684dce
naresh0839 <58386223+naresh0839@users.noreply.github.com>
2021-11-28 20:41:15 +0530

    Update settings.py

--- lorax/lorax/settings.py
+++ lorax/lorax/settings.py
@@ -141,5 +141,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "nicebharat00@gmail.com"
-EMAIL_HOST_PASSWORD = "nicebharatstores0022"
+EMAIL_HOST_PASSWORD = "Enter your password....."
 EMAIL_USE_TLS = True
